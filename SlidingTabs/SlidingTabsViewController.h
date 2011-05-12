15bb9d1ed996a8aa0826b2cb0ad0075b17b7b241
Mathew Piccinato <Mathew.Piccinato@phh.com>
2011-05-12 14:59:27 -0400

    Initial Commit

---
+++ SlidingTabs/SlidingTabsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SlidingTabsViewController.h
+//  SlidingTabs
+//
+//  Created by Mathew Piccinato on 5/12/11.
+//  Copyright 2011 PHH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlidingTabsViewController : UIViewController {
+    
+}
+
+@end
